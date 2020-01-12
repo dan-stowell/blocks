ca880b4a468109eebc4445cf3d1f8d7b17c7805d
Dan Stowell <danstowell@fastmail.com>
2020-01-12 03:09:01 +0000

    parse is-lines & grapple with evaluating expressions

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+class IsLine {
+  constructor(name, words) {
+    this.name = name;
+    this.words = words;
+  }
+
+  eval(env) {
+    let firstword = words[0];
+    let firstvalue = env.lookup(firstword);
+    let value = firstvalue;
+    for (word of this.words.slice(1)) {
+      if (value.knows(word)) {
+        value = value.apply(word);
+      } else if (env.has(word)) {
+        // also should check if current value accepts arguments
+        // because we'll be iterating over the remaining words,
+        // looking them up in the environment, and passing those
+        // values to the FunctionValue
+      } else {
+        // not sure what to do if we get here but flashing red
+        // probably is not the right move
+      }
+    }
+  }
+}
+
+let islineregex = new RegExp(/^\S+\s+is\s+\S.*$/);
+
 // Get the input box
 let input = document.getElementById('text');
 let output = document.getElementById('output');
@@ -14,11 +42,21 @@ input.addEventListener('keyup', function (e) {
 
     // Make a new timeout set to go off in 1000ms (1 second)
     timeout = setTimeout(function () {
-      let numblocks = input
+      let blocks = input
         .value
-        .split(new RegExp('\\n{2,}'))
+        .split(new RegExp('\\n{2,}'));
+
+      let numblocks = blocks
         .filter(block => block !== "")
         .length;
-      output.textContent = `${numblocks} blocks`;
+
+      let numislines = 0;
+      for (block of blocks) {
+        numislines += block
+          .split('\n')
+          .filter(line => islineregex.test(line))
+          .length;
+      }
+      output.textContent = `${numblocks} blocks, ${numislines} islines`;
     }, 1000);
 });
