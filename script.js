d036d6330a00cfbaa6d47a04bd63d20a471398c5
Dan Stowell <danstowell@fastmail.com>
2020-01-13 02:57:15 +0000

    can add two numbers. leaving in the console.log statements as a reminder of where i needed help!

--- script.js
+++ script.js
@@ -1,30 +1,121 @@
-class IsLine {
-  constructor(name, words) {
-    this.name = name;
-    this.words = words;
+class NumberValue {
+  constructor(number) {
+    console.log(`NumberValue ${number}`);
+    this.number = number;
   }
 
-  eval(env) {
-    let firstword = words[0];
-    let firstvalue = env.lookup(firstword);
-    let value = firstvalue;
-    for (word of this.words.slice(1)) {
-      if (value.knows(word)) {
-        value = value.apply(word);
-      } else if (env.has(word)) {
-        // also should check if current value accepts arguments
-        // because we'll be iterating over the remaining words,
-        // looking them up in the environment, and passing those
-        // values to the FunctionValue
-      } else {
-        // not sure what to do if we get here but flashing red
-        // probably is not the right move
-      }
+  moveright(word, blockenv, globalenv) {
+    if (word === 'plus') {
+      console.log(`PlusValue of ${this.number}`);
+      return new PlusValue(this.number);
+    } else {
+      return wordtovalue(word, blockenv, globalenv);
     }
   }
 }
 
+class PlusValue {
+  constructor(leftnumber) {
+    console.log(`PlusValue constructor ${leftnumber}`)
+    this.leftnumber = leftnumber;;
+  }
+
+  moveright(word, blockenv, globalenv) {
+    console.log(`PlusValue moveright ${this.leftnumber}`);
+    let rightnumber = wordtovalue(word, blockenv, globalenv);
+    let sum = this.leftnumber + rightnumber.number;
+    console.log(`sum ${sum}, rightnumber ${JSON.stringify(rightnumber)}`);
+    return new NumberValue(this.leftnumber + rightnumber.number);
+  }
+}
+
+function wordtovalue(word, blockenv, globalenv) {
+  console.log(`wordtovalue ${word}`);
+    if (blockenv.has(word)) {
+      return blockenv.lookup(word);
+    } else if (globalenv.has(word)) {
+      return globalenv.lookup(word);
+    } else if (!Number.isNaN(Number.parseInt(word))) {
+      return new NumberValue(Number.parseInt(word));
+    } else if (!Number.isNaN(Number.parseFloat(word))) {
+      return new NumberValue(Number.parseFloat(word));
+    } else {
+      // probably should return useful ErrorValue
+      return new EmptyValue();
+    }
+}
+
+class EmptyValue {
+  moveright(word, blockenv, globalenv) {
+    return wordtovalue(word, blockenv, globalenv);
+  }
+}
+
+// class IsLine {
+//   constructor(name, words) {
+//     this.name = name;
+//     this.words = words;
+//   }
+
+//   movedown(blockenv, globalenv) {
+//     let value = new EmptyValue();
+//     for (let word of this.words) {
+//       value = value.moveright(word, blockenv, globalenv)
+//     }
+//     blockenv.setvalue(this.name, value);
+//     return value;
+//   }
+// }
+
+class Line {
+  constructor(words) {
+    this.words = words;
+  }
+
+  movedown(blockenv, globalenv) {
+    let value = new EmptyValue();
+    for (let word of this.words) {
+      value = value.moveright(word, blockenv, globalenv);
+    }
+    return value;
+  }
+}
+
 let islineregex = new RegExp(/^\S+\s+is\s+\S.*$/);
+
+class Env {
+  constructor() {
+    this.mapping = {};
+  }
+
+  has(word) {
+    return (word in this.mapping);
+  }
+
+  lookup(word) {
+    return this.mapping[word];
+  }
+}
+
+// class FunctionValue {
+//   constructor(boundargs, args, lines) {
+//     this.boundargs = boundargs;
+//     this.args = args;
+//     this.lines = lines;
+//   }
+
+//   moveright(word, blockenv, globalenv) {
+//     if (this.args.length === 0) {
+//       value = this.movedownlines();
+//       return value.moveright(word, blockenv, globalenv);
+//     } else {
+//       arg = this.args[0];
+//       newboundargs = Object.assign({}, this.boundargs);
+//       newboundargs[arg] = wordtovalue(word, blockenv, globalenv);
+//       return new FunctionValue(newboundargs, this.args.slice(1), lines);
+//     }
+//   }
+// }
 
 // Get the input box
 let input = document.getElementById('text');
@@ -51,12 +142,22 @@ input.addEventListener('keyup', function (e) {
         .length;
 
       let numislines = 0;
-      for (block of blocks) {
-        numislines += block
-          .split('\n')
+      let values = [];
+      for (let block of blocks) {
+        let lines = block.split('\n');
+        numislines += lines
           .filter(line => islineregex.test(line))
           .length;
+        let newvalues = lines
+          .filter(line => !islineregex.test(line))
+          .map(line => {
+            let words = line
+              .split(new RegExp('\\s+'));
+            let value = (new Line(words)).movedown(new Env(), new Env());
+            return value;
+          });
+        values = values.concat(newvalues);
       }
-      output.textContent = `${numblocks} blocks, ${numislines} islines`;
+      output.textContent = `${JSON.stringify(values)} // ${numblocks} blocks, ${numislines} islines`;
     }, 1000);
 });
