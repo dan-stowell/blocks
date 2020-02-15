2f1a50d29a349f37832d863fbf89e397e2ddc8f6
Dan Stowell <danstowell@fastmail.com>
2020-02-15 02:24:16 +0000

    parse blocks in web worker; still throwing spurious exception

--- parseblocks.js
+++ parseblocks.js
@@ -1,4 +1,69 @@
+importScripts('md5.min.js');
+
+let islineregex = new RegExp(/^\s*(\S+)\s+((\S+\s+)*)is\s+(.*)$/);
+let blockislineregex = new RegExp(/^\s*(\S+)\s+((\S+\s+)*)is$/);
+
+function parseblocks(inputtext) {
+  let version = md5(inputtext);
+
+  let blocks = inputtext
+    .split(new RegExp('\\n{2,}'));
+
+  let parsedlines = [];
+
+  blocks.forEach((block, whichblock) => {
+    let lines = block.split('\n');
+
+    lines.forEach((line, whichline) => {
+      if (islineregex.test(line)) {
+        let match = islineregex.exec(line);
+        let name = match[1];
+        let args = match[2]
+          .trim()
+          .split(new RegExp('\\s+'))
+          .filter(word => word !== '');
+        let restofline = match[3];
+        parsedlines.push({
+          'name': name,
+          'args': args,
+          'lines': [restofline],
+          'whichblock': whichblock,
+          'whichline': whichline
+        });
+
+      } else if (blockislineregex.test(line)) {
+        let match = blockislineregex.exec(line);
+        let name = match[1];
+        let args = match[2]
+          .trim()
+          .split(new RegExp('\\s+'))
+          .filter(word => word !== '');
+        let restoflines = lines.slice(whichline + 1);
+        parsedlines.push({
+          'name': name,
+          'args': args,
+          'lines': restoflines,
+          'whichblock': whichblock,
+          'whichline': whichline
+        });
+
+      } else {
+        parsedlines.push({
+          'name': '',
+          'args': [],
+          'lines': [line],
+          'whichblock': whichblock,
+          'whichline': whichline
+        });
+
+      }
+    });
+
+  });
+  postMessage({'version': version, 'parsedlines': parsedlines});
+}
+
 onmessage = function(e) {
-  let message = `worker received version ${e.data}`;
-  postMessage(message);
+  let parsed = parseblocks(e.data);
+  postMessage(parsed);
 }
--- script.js
+++ script.js
@@ -1,86 +1,23 @@
-let islineregex = new RegExp(/^\s*(\S+)\s+((\S+\s+)*)is\s+(.*)$/);
-let blockislineregex = new RegExp(/^\s*(\S+)\s+((\S+\s+)*)is$/);
 
-// Get the input box
 let input = document.getElementById('text');
 let output = document.getElementById('output');
 
-// Init a timeout variable to be used below
-let timeout = null;
-
-let versions = [];
-let version2blocks = {};
-let version2blockparsedlines = {};
-
 let parseblocksworker = new Worker('parseblocks.js');
 parseblocksworker.onmessage = function(e) {
-  console.log(`main received message: ${e.data}`);
+  let parsed = e.data;
+  let version = parsed['version'];
+  let parsedlines = parsed['parsedlines'];
+  let summary = `${parsedlines.length} parsed lines, version ${version}`;
+  output.textContent = summary;
 };
 
-function parseblocks() {
-  let version = md5(input.value)
-  parseblocksworker.postMessage(version);
-
-  let blocks = input
-    .value
-    .split(new RegExp('\\n{2,}'));
-
-  versions.push(version);
-  version2blocks[version] = blocks;
-  version2blockparsedlines[version] = [];
-
-  let numblocks = blocks
-    .filter(block => block !== "")
-    .length;
-
-  let numlines = 0;
-  let numparsedlines = 0;
-  let blockparsedlines = [];
-  blocks.forEach((block, whichblock) => {
-    let lines = block.split('\n');
-    let parsedlines = [];
-
-    lines.forEach((line, whichline) => {
-      numlines += 1;
-      if (islineregex.test(line)) {
-        let match = islineregex.exec(line);
-        let name = match[1];
-        let args = match[2]
-          .trim()
-          .split(new RegExp('\\s+'))
-          .filter(word => word !== '');
-        let restofline = match[3];
-        parsedlines.push({'name': name, 'args': args, 'lines': [restofline]});
-
-      } else if (blockislineregex.test(line)) {
-        let match = blockislineregex.exec(line);
-        let name = match[1];
-        let args = match[2]
-          .trim()
-          .split(new RegExp('\\s+'))
-          .filter(word => word !== '');
-        let restoflines = lines.slice(whichline + 1);
-        parsedlines.push({'name': name, 'args': args, 'lines': restoflines});
-
-      } else {
-        parsedlines.push({'name': '', 'args': [], 'lines': [line]});
-
-      }
-    });
-
-    numparsedlines += parsedlines.length;
-    version2blockparsedlines[version].push(parsedlines);
-  });
-  output.textContent = `${numlines} lines, ${numparsedlines} parsed lines, ${versions.length} versions, version ${version}`;
+function postparseblocks() {
+  console.log('postparseblocks');
+  parseblocksworker.postMessage(input.value);
 }
 
-// Listen for keystroke events
+let timeout = null;
 input.addEventListener('keyup', function (e) {
-    // Clear the timeout if it has already been set.
-    // This will prevent the previous task from executing
-    // if it has been less than <MILLISECONDS>
-    clearTimeout(timeout);
-
-    // Make a new timeout set to go off in 1000ms (1 second)
-    timeout = setTimeout(parseblocks, 70);
+  clearTimeout(timeout);
+  timeout = setTimeout(postparseblocks, 70);
 });
